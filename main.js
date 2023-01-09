2fb929ce3f10416cbd82f340dae04f948720ed19
Darth-Ness <reeuuthdc80j@opayq.net>
2023-01-09 10:11:07 -0500

    Enhancement: Majorly improve UI and functionatly

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var toCalculate = ""; 
-var display = document.getElementById('display')
+var display = document.getElementById('display');
+function clearScreen() {
+    toCalculate = "";
+    display.innerHTML = "";
+    console.log(display.innerHTML);
+}
 function calculate() {
     display.innerHTML = eval(toCalculate);
     toCalculate = "";
@@ -8,7 +13,11 @@ function add(char){
     toCalculate+=char;
     display.innerHTML = toCalculate;
 }
-function clear() {
-    toCalculate = "";
-    display.innerHTML = "";
+function deleteLastChar() {
+    toCalculate = toCalculate.slice(0,toCalculate.length-1);
+    display.innerHTML = toCalculate;
+}
+function root() {
+    display.innerHTML = Math.sqrt(display.innerHTML);
+    toCalculate = display.innerHTML;
 }
